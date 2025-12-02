09bb5341c07c52ef1610cf3272912a1aa983db0e
Barışcan Çeken <bariscanceken@hotmail.com>
2025-12-02 11:57:23 +0300

    lunar lander project completed.

---
+++ LunarLander/train.py
@@ -0,0 +1,32 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+import os
+
+model_name = "ppo-LunarLander-v3"
+env_id = "LunarLander-v3"
+
+os.makedirs("models", exist_ok=True)
+os.makedirs("logs", exist_ok=True)
+
+env = gym.make(env_id, render_mode=None)
+
+print("model creating...")
+model = PPO(
+    policy="MlpPolicy",
+    env=env,
+    n_steps=1024,
+    batch_size=64,
+    n_epochs=4,
+    gamma=0.999,
+    gae_lambda=0.98,
+    ent_coef=0.01,
+    verbose=1,
+)
+
+print("train starting...")
+model.learn(total_timesteps=100000)
+
+save_path = f"models/{model_name}"
+model.save(save_path)
+
+print(f"model saved to: {save_path}.zip")
